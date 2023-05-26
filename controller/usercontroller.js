c9d58618b2275fc0cedf544fb5dbb3540a0b5526
Inko03 <99274731+Inko03@users.noreply.github.com>
2023-05-26 02:25:42 +0200

    Update usercontroller.js

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -2,21 +2,27 @@ const User = require('../models/addNewUser')
 const { json } = require('express')
 
 const postUser = async(req,res)=>{
-    const user = await User.create({
-        email:req.body.email,
-        password:req.body.password,
-    })
-    res.status(200).json(user)
+    const user = await User.find({email:req.body.email})
+    const isuser = user.length
+    if(isuser===1){
+        res.status(400).json({message:"USER-EXIST"})
+    }else{
+        const newuser = await User.create({
+            email:req.body.email,
+            password:req.body.password
+        })
+        res.status(200).json({message:"NEW-USER-ADD"})
+    }
 }
 const getUser = async(req,res)=>{
     const user = await User.find({email:req.body.email})
     const isUser = user.length
     if(isUser===0){
-        res.status(200).json({message:'No such user'})
+        res.status(400).json({message:'NO-USER'})
     }else if(user[0].password===req.body.password){
         res.status(200).json(user)
     }else{
-        res.status(400).send({message:'Wrong password'})
+        res.status(400).send({message:'WRONG-PASSWORD'})
     }
 }
 module.exports={
